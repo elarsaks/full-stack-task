be5a847b4bc682d7940f67ed2c44f67e6a6a11a6
elarsaks <elarsaks@gmail.com>
2021-08-28 16:31:32 +0300

    Back end working in Docker.

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const PORT = process.env.PORT || 3001
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +8,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(PORT, function () {
+  console.log('Api listening on port:', PORT)
+})
