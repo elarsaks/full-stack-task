708beded05605575c92d4c06c18de3fb53a6c8b0
elarsaks <elarsaks@gmail.com>
2021-08-29 01:51:56 +0300

    Get two way data binding working.

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const FormElement = styled.form`
@@ -39,77 +40,56 @@ interface FormProps {
 	activeBook: Book
 }
 
-class Form extends React.Component<FormProps, Book> {
+const Form: React.FC<FormProps> = ({ activeBook }) => {
 
-	constructor(props: FormProps) {
-		super(props);
+	const [book, setBook] = useState<Book>({
+		id: 0, // NOTE: this is just a placeholder, real ID will come from SQL
+		title: '',
+		author: '',
+		description: ''
+	})
 
-		this.state = {
-			id: 99999999, // NOTE: this is just a placeholder, real ID will come from SQL
-			title: '',
-			author: '',
-			description: ''
-		}
-
-		this.onInputchange = this.onInputchange.bind(this)
+	function onInputchange(event: any) {
+		setBook({ ...book, [event.target.name]: event.target.value })
 	}
 
-	onInputchange = (event: any) => {
-		switch (event.target.name) {
-			case 'title':
-				this.setState((state) => ({ title: event.target.value }))
-				break;
-			case 'author':
-				this.setState((state) => ({ author: event.target.value }))
-				break;
-			case 'description':
-				this.setState((state) => ({ description: event.target.value }))
-				break;
-			default:
-		}
-		// TODO: Why it cannot be done like this?
-		//this.setState(state => ({ [event.target.name]: event.target.value }))
-	}
+	useEffect(() => setBook(activeBook), [activeBook])
+	return (
+		<FormElement >
+			<label>
+				<h4 >Title:</h4>
+				<Input
+					name='title'
+					value={book.title}
+					onChange={onInputchange}
+				/>
+			</label>
 
+			<label>
+				<h4 >Author:</h4>
+				<Input
+					name='author'
+					value={book.author}
+					onChange={onInputchange}
+				/>
+			</label>
 
+			<label>
+				<h4 >Description</h4>
+				<TextArea
+					name='description'
+					value={book.description}
+					onChange={onInputchange}
+				/>
+			</label>
 
-	render() {
-		return (
-			<FormElement >
-				<label>
-					<h4 >Title:</h4>
-					<Input
-						name='title'
-						value={this.state.title}
-						onChange={this.onInputchange}
-					/>
-				</label>
-
-				<label>
-					<h4 >Author:</h4>
-					<Input
-						name='author'
-						value={this.state.author}
-						onChange={this.onInputchange}
-					/>
-				</label>
-
-				<label>
-					<h4 >Description</h4>
-					<TextArea
-						name='description'
-						onChange={this.onInputchange}
-					/>
-				</label>
-
-				<div>
-					<Button >Save New</Button>
-					<Button >Save</Button>
-					<Button >Delete</Button>
-				</div>
-			</FormElement >
-		)
-	}
+			<div>
+				<Button >Save New</Button>
+				<Button >Save</Button>
+				<Button >Delete</Button>
+			</div>
+		</FormElement >
+	)
 };
 
 export default Form;
